355198ad2c80ed797640019425b9320cce604372
Suman Kolisetty <skolisety@dotloop.com>
2015-02-03 15:05:08 -0800

    First sample program

---
+++ 100A-Assignments/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  100A-Assignments
+//
+//  Created by Suman Kolisetty on 2/3/15.
+//  Copyright (c) 2015 Satya Kolla. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hi Sneha, I have created new github repository. Please create your projects under this and run this one only all the time!\n";
+    return 0;
+}
