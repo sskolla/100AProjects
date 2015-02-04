d82fa239c3e8b85f4d5e159f7c4fc2c681d53871
Suman Kolisetty <skolisetty@dotloop.com>
2015-02-03 16:11:22 -0800

    Adding second file addition.cpp

---
+++ 100A-Assignments/addition.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void addition() {
+    // insert code here...
+    float x,y;
+    cout << "Enter both values"<< endl;
+    cin >> x;
+    cin >> y;
+    cout << "Value after adding both is " << x+y;
+    return 0;
+}
